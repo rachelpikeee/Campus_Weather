04402cacdc8cde221e92679c0ec6517ccbe006a5
rachelpikeee <70335369+rachelpikeee@users.noreply.github.com>
2021-02-17 20:15:17 -0500

    Update Data_Organize.R

---
+++ Data_Organize.R
@@ -0,0 +1,18 @@
+# Started 2/17/2021
+# Script to read in, clean, and organize campus weather data
+
+#### Setting up directories
+
+# Creating user numbers for each person
+Users = c(1, # Rachel
+          2) # Professor Kropp 
+
+DirMeter = c("/Volumes/GoogleDrive/.shortcut-targets-by-id/1sRKN7b9U7odoX9ABwoUqoeY1G-kRMjHC/campus_weather/METER/",
+              )
+DirTOMST = c("/Volumes/GoogleDrive/.shortcut-targets-by-id/1sRKN7b9U7odoX9ABwoUqoeY1G-kRMjHC/campus_weather/TOMST/",
+              )
+
+# Select user - change if needed
+user = 1
+
+
