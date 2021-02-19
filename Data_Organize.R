b2c045d68516b1c6093f4c4ea631c96c7f914d24
rachelpikeee <70335369+rachelpikeee@users.noreply.github.com>
2021-02-19 08:43:27 -0500

    Update Data_Organize.R

--- Data_Organize.R
+++ Data_Organize.R
@@ -20,29 +20,32 @@ DirTOMST = c("/Volumes/GoogleDrive/.shortcut-targets-by-id/1sRKN7b9U7odoX9ABwoUq
 user = 1
 
 
-#### Reading in Data ----
-ColNames <- c("Date",
-              "SolRad",
-              "Precip",
-              "LightningAct",
-              "LightningDist",
-              "WindDir",
-              "WindSpeed",
-              "GustSpeed",
-              "AirTemp",
-              "VaporPr",
-              "AtmosPr",
-              "XLevel",
-              "YLevel",
-              "MaxPrecip",
-              "SensorTemp",
-              "VPD",
-              "BatPct",
-              "BatVolt",
-              "RefPr",
-              "LogTemp")
+#### Reading in Meter Data ----
+
+# Only run ONCE when setting up data frames!!
+# Creating large data frame for all data from meter sensor 
+# First create column names vector
+ColNames <- c("Date","SolRad","Precip","LightningAct","LightningDist","WindDir","WindSpeed",
+              "GustSpeed","AirTemp","VaporPr","AtmosPr","XLevel","YLevel","MaxPrecip",
+              "SensorTemp","VPD","BatPct","BatVolt","RefPr","LogTemp")
+
+# Create empty data frame and rename columns
 MeterData <- data.frame(matrix(ncol = 20, nrow = 0))
-colnames(MeterData) <- ColNames                     
+colnames(MeterData) <- ColNames
+
+
+# Reading in most recent data -- RUN EVERY TIME NEW DATA IS DOWNLOADED
+
+# Character string of Date of Last Download -> change this every time
+#   Make sure it is the format DayMonYear-Time.csv (ex: 07Jan21-1510.csv)
+#   This should match the name of the file for the data on Google Drive
+DOLD <- "07Jan21-1510.csv"
+
+# Read in the csv, skipping the first three columns
+MeterData <- read.csv(paste0(DirMeter[user], DOLD), header = FALSE, skip = 3)
+
+# Rename columns
+colnames(MeterData) <- ColNames
 
 #### Cleaning Data ----
 
@@ -50,7 +53,6 @@ colnames(MeterData) <- ColNames
 #### Base Analysis of Data ----
 
 #### Metadata ----
-UnitInfo <- data.frame()
-
-
-
+MeterUnits <- data.frame(t(c("Date", "W/m^2","mm","N/A","km","˚","m/s","m/s","˚C",
+                           "kPa","kPa","˚","˚","mm/h","˚C","kPa","%","mV","kPa","˚C")))
+colnames(MeterUnits) <- ColNames 
