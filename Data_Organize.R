98f3fd9fd0ef73f7b054cade165c9a6fd43d69c4
rachelpikeee <70335369+rachelpikeee@users.noreply.github.com>
2021-02-21 20:48:06 -0500

    Update Data_Organize.R

--- Data_Organize.R
+++ Data_Organize.R
@@ -2,12 +2,13 @@ library(dplyr)
 library(tidyr)
 library(lubridate)
 library(naniar)
+library(tibble)
 
 # Started 2/17/2021
 # Script to read in, clean, and organize campus weather data
 
 # to do:
-#   organize by what needs to be done every time and what should only be done once
+#   - organize by what needs to be done every time and what should only be done once
 
 #### Setting up directories
 
@@ -23,67 +24,22 @@ DirTOMST = c("/Volumes/GoogleDrive/.shortcut-targets-by-id/1sRKN7b9U7odoX9ABwoUq
 # Select user - change if needed
 user = 1
 
-
-#### Reading in Meter Data ----
-
-# Only run ONCE when setting up data frames!!
+#### ONLY RUN ONE TIME ----
 # Creating large data frame for all data from meter sensor 
 # First create column names vector
 ColNames <- c("Date","SolRad","Precip","LightningAct","LightningDist","WindDir","WindSpeed",
               "GustSpeed","AirTemp","VaporPr","AtmosPr","XLevel","YLevel","MaxPrecip",
-              "SensorTemp","VPD","BatPct","BatVolt","RefPr","LogTemp")
+              "SensorTemp","VPD","BatPct","BatVolt","RefPr","LogTemp", "sFlag", 
+              "lFlag", "exFlag")
 
 # Create empty data frame and rename columns
-MeterData <- data.frame(matrix(ncol = 20, nrow = 0))
+MeterData <- data.frame(matrix(ncol = length(ColNames), nrow = 0))
 colnames(MeterData) <- ColNames
 
-# Reading in most recent data -- RUN EVERY TIME NEW DATA IS DOWNLOADED
-NewMeterData <- data.frame(matrix(ncol = 20, nrow = 0))
-colnames(NewMeterData) <- ColNames
-
-# Character string of Date of Last Download -> change this every time
-#   Make sure it is the format DayMonYear-Time.csv (ex: 07Jan21-1510.csv)
-#   This should match the name of the file for the data on Google Drive
-DOLD <- "07Jan21-1510.csv"
-
-# Read in the csv, skipping the first three columns
-NewMeterData <- read.csv(paste0(DirMeter[user], DOLD), header = FALSE, skip = 3)
-# Rename columns
-colnames(NewMeterData) <- ColNames
-# Cleaning up NA values
-NewMeterData <- replace_with_na_all(NewMeterData, condition = ~.x == "#N/A")
-
-#### Cleaning Data ----
-# Create total NA variable
+# Create total NA variable to help keep track of sensor's QC tactics
 total_NA <- 0
-# Count NAs in new data in one row - RUN EACH TIME
-new_NA <- as.numeric(sum(is.na(NewMeterData$SolRad)))
-# add to total - RUN EACH TIME
-total_NA <- total_NA + new_NA
 
-# remove NAs from data frame
-NewMeterData <- drop_na(NewMeterData)
-
-# Rounds of quality control
-# round 1 = based on metadata / field observations / user assessment
-#   ex: weather station broken --> remove data completely
-#   ex: snow/ice/other things covering the sensor --> flag
-#   ex: weird spikes/abnormal data --> need to see if something happened
-#   NOTE: meter data has own QC checks --> look for NA's in the data
-# if there are any that have these types, go through data more manually to fix problem
-
-# Meter data steps
-# Read in data - both in own table and to old table
-# Create table with date of download/observation and user notes
-# Count NA's since last download and cumulatively (its own QC checks)
-# Check for extreme values / spikes in the data
-
-# Separate script for TOMST data -- more complicated
-
-#### Combine Data Frames ----
-
-#### Metadata ----
-# Creating data frame for field notes -> ONLY RUN ONCE
+# Creating data frame for field notes
 #   DATE - date of data download
 #   TIME - time of data download
 #   GEN CONDITOINS - General weather, snow, other conditions from trip
@@ -94,24 +50,84 @@ colnames(FieldNotes) <- c("Date", "General Conditions", "Meter Conditions",
                           "TOMST Conditions")
 FieldNotes$Date <- as_datetime(FieldNotes$Date)
 
-# ADD FIELD OBSERVATIONS HERE - DO EVERY TIME YOU DOWNLOAD
+# Creating data frame to keep track of units for Meter Data
+MeterUnits <- data.frame(ColNames)
+MeterUnits$Units <- c("Date", "W/m^2","mm",NA,"km","˚","m/s","m/s","˚C",
+                      "kPa","kPa","˚","˚","mm/h","˚C","kPa","%","mV","kPa","˚C",
+                      NA, NA, NA)
+colnames(MeterUnits) <- c("Measurement", "Units")
+
+################################################################################
+
+#### RUN EVERY TIME NEW DATA IS DOWNLOADED ----
+# Adding field observations to data frame
 # Keeping track of current row - MAKE SURE TO RUN!!!
 current_row = nrow(FieldNotes)+1
 # Change the date to most recent download - keep the same format!
-FieldNotes[current_row, "Date"] <- as_datetime("2021-2-7 15:00:00", tz = "EST")
+FieldNotes[current_row, "Date"] <- as_datetime("2020-12-19 12:00:00", tz = "EST")
 # Add field observations here
-FieldNotes[current_row, "General Conditions"] <-"Sunny with about .3m snow"
+FieldNotes[current_row, "General Conditions"] <-"Sunny with about .4m of snow"
 FieldNotes[current_row, "Meter Conditions"] <- "Good conditions"
-FieldNotes[current_row, "TOMST Conditions"] <- "Only .5m sensor visible, good conditions"
+FieldNotes[current_row, "TOMST Conditions"] <- "Only .5m sensor visible above snow"
 
-# Creating data frame to keep track of units for Meter Data
-MeterUnits <- data.frame(ColNames)
-MeterUnits$Units <- c("Date", "W/m^2","mm","N/A","km","˚","m/s","m/s","˚C",
-                      "kPa","kPa","˚","˚","mm/h","˚C","kPa","%","mV","kPa","˚C")
-colnames(MeterUnits) <- c("Measurement", "Units")
+# Reading in most recent data
+# Character string of Date of Last Download -> change this every time
+#   Make sure it is the format DayMonYear-Time.csv (ex: 07Jan21-1510.csv)
+#   This should match the name of the file for the data on Google Drive
+DOLD <- "07Jan21-1510.csv"
 
+# Read in the csv, skipping the first three columns
+NewMeterData <- read.csv(paste0(DirMeter[user], DOLD), header = FALSE, skip = 3)
+# Cleaning up NA values
+NewMeterData <- replace_with_na_all(NewMeterData, condition = ~.x == "#N/A")
+# Count NAs in new data (only use one row to count NA observations not total 
+#                        NA cells)
+new_NA <- as.numeric(sum(is.na(NewMeterData$V2)))
+# add to total
+total_NA <- total_NA + new_NA
+# remove NAs from data frame
+NewMeterData <- drop_na(NewMeterData)
+# Add flag columns
+NewMeterData <- add_column(.data = NewMeterData, sFlag = NA, lFlag = NA, exFlag = NA)
+# Rename columns
+colnames(NewMeterData) <- ColNames
 
+# Making each column the appropriate type of data -- cant convert to datetime without seconds
+NewMeterData$Date <- as_datetime(NewMeterData$Date, tz = "EST")
+NewMeterData$SolRad <- as.numeric(NewMeterData$SolRad)
+NewMeterData$Precip <- as.numeric(NewMeterData$Precip)
+NewMeterData$LightningAct <- as.numeric(NewMeterData$LightningAct)
+NewMeterData$LightningDist <- as.numeric(NewMeterData$LightningDist)
+NewMeterData$WindDir <- as.numeric(NewMeterData$WindDir)
+NewMeterData$WindSpeed <- as.numeric(NewMeterData$WindSpeed)
+NewMeterData$GustSpeed <- as.numeric(NewMeterData$GustSpeed)
+NewMeterData$AirTemp <- as.numeric(NewMeterData$AirTemp)
+NewMeterData$VaporPr <- as.numeric(NewMeterData$VaporPr)
+NewMeterData$AtmosPr <- as.numeric(NewMeterData$AtmosPr)
+NewMeterData$XLevel <- as.numeric(NewMeterData$XLevel)
+NewMeterData$YLevel <- as.numeric(NewMeterData$YLevel)
+NewMeterData$MaxPrecip <- as.numeric(NewMeterData$MaxPrecip)
+NewMeterData$SensorTemp <- as.numeric(NewMeterData$SensorTemp)
+NewMeterData$VPD <- as.numeric(NewMeterData$VPD)
 
-# missing data from 12/12 - 12/19 = weather station unplugged
+# Adding flags for level
+for (i in 1:nrow(NewMeterData)){
+  NewMeterData$lFlag[i] <- ifelse(NewMeterData[i, "XLevel"]>2 | NewMeterData[i, "XLevel"]<(-2)
+                               | NewMeterData[i, "YLevel"]>2 | NewMeterData[i, "YLevel"]<(-2),
+                               "X", NA)
+}
+  
+# TO DO:
+# Add method for snow and extreme value flags
+# Combine new meter data with big data set
+# Check for overlap
+# Save files google drive
 
-# Save final clean data tables (data, metadata, etc.) back into the google drive folder
+# QUESTIONS:
+#   changing date as datetime format -- no seconds, function doesn't work
+#   easier way to make all columns numeric?
+
+# NOTES:
+#   Separate script for TOMST data -- more complicated
+#   Save final clean data tables (data, metadata, etc.) back into the google drive folder
+#   Add some check for clock / time of observation -> need to think about this more
