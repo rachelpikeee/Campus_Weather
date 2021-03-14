6e863267a8becc47a355c667d28447cf30b7899c
rachelpikeee <70335369+rachelpikeee@users.noreply.github.com>
2021-03-14 15:41:41 -0400

    Added source function to user_script

--- Data_Organize.R
+++ Data_Organize.R
@@ -28,7 +28,7 @@ user = 1
 
 # Creating marker for initial run --> if this is true the first section of code
 #   will run, if false the second section will run.
-InitialRunFlag <- FALSE
+InitialRunFlag <- UserInputs[1,2]
 
 #### ONLY RUN ONE TIME ----
 if (InitialRunFlag == TRUE){
@@ -72,7 +72,8 @@ colnames(UserInputsAll) <- UserCols
 # Character string of Date of Last Download -> change this every time
 #   Make sure it is the format DayMonYear-Time.csv (ex: 07Jan21-1510.csv)
 #   This should match the name of the file for the data on Google Drive
-DOLD <- "07Jan21-1510.csv"
+
+DOLD <- UserInputs[1,3]
 
 # Read in the csv, skipping the first three columns
 NewMeterData <- read.csv(paste0(DirMeter[user], DOLD), header = FALSE, skip = 3,
@@ -110,12 +111,11 @@ CountNA <- function(Date){
   NAcount[i,5] <<- sum(NAcount[i,2:4])
 }
 
-# Function works!!
-CountNA("12/11/20 14:30")
+# Use function
+CountNA(UserInputs[1,4])
 
 
 # Adding flags for level
-# Should I make this a function?
 for (i in 1:nrow(NewMeterData)){
   NewMeterData$lFlag[i] <- ifelse(NewMeterData[i, "XLevel"]>2 | NewMeterData[i, "XLevel"]<(-2)
                                | NewMeterData[i, "YLevel"]>2 | NewMeterData[i, "YLevel"]<(-2),
@@ -123,32 +123,24 @@ for (i in 1:nrow(NewMeterData)){
 }
 
 # Adding flag for snow
-# Start and end dates for period with snow on sensor (change these values in user script
-#     based on field notes)
+# Start and end dates for period with snow on sensor 
 # Snow flag function
 SnowFlag <- function(start_date, end_date, tz){
   sFlag1 = rep(NA, nrow(NewMeterData))
-  if (tz == "EST"){
-    for (i in 1:length(start_date)){
-      sFlag1 <- ifelse(NewMeterData$Date_Format >= mdy_hm(start_date[i]) & 
+  for (i in 1:length(start_date)){
+    sFlag1 <- ifelse(NewMeterData$Date_Format >= mdy_hm(start_date[i]) & 
                      NewMeterData$Date_Format <= mdy_hm(end_date[i]), 
                      1, sFlag1)
       
-    }
-    NewMeterData$sFlag <<- sFlag1
-  } else{
-  print("Error: wrong time zone, use EST for data entry")
   }
+  NewMeterData$sFlag <<- sFlag1
 }
 
-# Function works now!
-SnowFlag(c("12/11/20 14:30", "1/1/21 14:30"), 
-         c("12/13/20 14:30", "1/3/21 14:30"),
-         "EST")
+# Use Function
+SnowFlag(UserInputs[1,5], UserInputs[1,6], UserInputs[1,7])
 
 
 # Adding flag for extreme temp values
-# should these be functinos?
 for (i in 1:nrow(NewMeterData)){
   NewMeterData$TempFlag[i] <- ifelse(NewMeterData[i, "AirTemp"]>100 | NewMeterData[i, "AirTemp"]<(-100),
                                   "X", NA)
@@ -166,37 +158,31 @@ for (i in 1:nrow(NewMeterData)){
                                      "X", NA)
 }
 
-
-# Combining new meter data with larger data frame (not tested)
+# Combining new meter data with larger data frame
 MeterData <- rbind(MeterData, NewMeterData)
 
-# Checking for overlap (note tested)
+# Getting rid of overlap
 MeterData <- MeterData[!duplicated(MeterData$Date), ]
 
+# Combining recent user inputs with complete data frame
+UserInputsAll <- rbind(UserInputsAll, UserInputs)
+
 # Saving all files back into Google Drive
-# save files as different versions -- don't rewrite over the old files
-# allows for better workflow
-# user defined variable for version name (make this a function)
 # Meter data
-write.csv(MeterData, paste0(DirFinal[user], "/MeterData.csv"))
+write.csv(MeterData, paste0(DirFinal[user], "/MeterData", UserInputs[1, 1], ".csv"))
 # Meter unit data
 # move to top -- only needs to be run once
-write.csv(MeterUnits, paste0(DirFinal[user], "/MeterUnits.csv"))
+write.csv(MeterUnits, paste0(DirFinal[user], "/MeterUnits", UserInputs[1, 1], ".csv"))
 # NA Count
-write.csv(NAcount, paste0(DirFinal[user], "/NAcount.csv"))
+write.csv(NAcount, paste0(DirFinal[user], "/NAcount", UserInputs[1, 1], ".csv"))
+# User Inputs
+write.csv(UserInputsAll, paste0(DirFinal[user], "/UserInputsAll", UserInputs[1, 1], ".csv"))
 }
 
 # TO DO:
-# make saving file a function
 # add checks for incorrect inputs throughout script --> print errors
-# rbind the user input data to the user input all
-# add version names for files
 # test with two data sets that we have
 # test with errors
 
 # QUESTIONS:
 
-
-# NOTES:
-#   Separate script for TOMST data -- more complicated
-#   Add some check for clock / time of observation -> need to think about this more
--- User_Script.R
+++ User_Script.R
@@ -1,10 +1,12 @@
 # USER SCRIPT FOR DOWNLOADING DATA
 
-#### Setting up directories
-
 # Creating user numbers for each person
 Users = c(1, # Rachel
           2) # Professor Kropp 
+
+# File path for data_organize code
+DirCode = c("/Users/rachelpike/Desktop/GitHub/Campus_Weather",
+            "")
 
 # File path for meter data
 DirMeter = c("/Volumes/GoogleDrive/.shortcut-targets-by-id/1sRKN7b9U7odoX9ABwoUqoeY1G-kRMjHC/campus_weather/METER/",
@@ -27,38 +29,36 @@ colnames(UserInputs) <- UserCols
 # UPDATE INPUTS HERE!!!!
 # Version: This is the title of this version of data. Can be anything, but 
 #          could follow format 1.1, or v1, v2.1, etc. 
-UserInputs[1,1] <- "v1"
+UserInputs[1,1] <- "v3"
 # InitialRunFlag: TRUE or FALSE, make TRUE if this is the first time the script is
 #                 run, otherwise put false
-UserInputs[1,2] <- TRUE
+UserInputs[1,2] <- FALSE
 # DOLD: Date of last download, this is to read in the data, make sure it is in the
 #       format format DayMonYear-Time.csv (ex: 07Jan21-1510.csv). This should 
 #       match the name of the file for the data on Google Drive.
-UserInputs[1,3] <- "07Jan21-1510.csv"
+UserInputs[1,3] <- "26Feb21-1351.csv"
 # NADate: This is the same date as day of last download. It will go into the data
 #         frame that keeps track of NA values. The format for this date is 
 #         mm/dd/yy hh:mm (ex: 12/11/20 14:30)
-UserInputs[1,4] <- "12/11/20 14:30"
+UserInputs[1,4] <- "02/26/21 13:51"
 # SnowStart: If in your field notes, you indicate a period where the sensor was 
 #            covered by snow or ice, this is the start date for that period. The
 #            format for this date is mm/dd/yy hh:mm (ex: 12/11/20 14:30)
-UserInputs[1,5] <- "12/11/20 14:30"
+UserInputs[1,5] <- NA
 # SnowEnd: If in your field notes, you indicate a period where the sensor was 
 #            covered by snow or ice, this is the end date for that period. The
 #            format for this date is mm/dd/yy hh:mm (ex: 12/11/20 14:30)
-UserInputs[1,6] <- "12/11/20 14:30"
+UserInputs[1,6] <- NA
 # TZ: This is the time zone for the observations. Should be either "EST" or "EDT"
 #     depending on daylight savings. Between March-November, use EDT.
 UserInputs[1,7] <- "EST"
 
 
-
-# use the source function
-#   acts like a copy and paste
-# like highlighting the entire script and hitting run
-# specify the variables that need to be defined before source
+# Run Data_Organize script
+source(paste0(DirCode[user], "/Data_Organize.R"))
 
 
+# TO DO:
 # tests for mistakes
 # print errors as the script runs
 
